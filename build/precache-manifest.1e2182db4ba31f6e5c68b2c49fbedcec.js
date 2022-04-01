3f2316b2bee74e961dc16723ba5a762cef52bc75
Alagbu somto <s.alagbu.com>
2022-04-01 10:41:59 +0100

    Ran Build

---
+++ build/precache-manifest.1e2182db4ba31f6e5c68b2c49fbedcec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f03785fce51f909a051430c9c41137c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "20b0b6fd942011fd4669",
+    "url": "/my-app/static/css/2.75ff4ca6.chunk.css"
+  },
+  {
+    "revision": "20b0b6fd942011fd4669",
+    "url": "/my-app/static/js/2.5393d2a8.chunk.js"
+  },
+  {
+    "revision": "0c96b4204977f768bd23",
+    "url": "/my-app/static/js/main.99886812.chunk.js"
+  },
+  {
+    "revision": "d8c80200965c68416152",
+    "url": "/my-app/static/js/runtime-main.31999df7.js"
+  }
+]);
